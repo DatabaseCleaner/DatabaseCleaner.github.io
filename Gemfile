c1c3bbd8c946d19a73b92ba16ecd98ca07d81a2f
Jon Rowe <hello@jonrowe.co.uk>
2014-12-18 09:15:04 +1100

    switch to middleman deploy

--- Gemfile
+++ Gemfile
@@ -1,7 +1,11 @@
 source "https://rubygems.org"
 
-gem "middleman", "~> 3.3.7"
+gem "middleman",      "~> 3.3.7"
 gem "middleman-blog", "~> 3.5.3"
+gem "middleman-livereload", "~> 3.3.4"
+gem "middleman-syntax", :github => "middleman/middleman-syntax"
+gem "middleman-favicon-maker"
+gem "middleman-deploy"
 
 # For feed.xml.builder
 gem "builder", "~> 3.0"
--- config.rb
+++ config.rb
@@ -1,103 +1,29 @@
-###
-# Blog settings
-###
-
-# Time.zone = "UTC"
-
 activate :blog do |blog|
-  # This will add a prefix to all links, template references and source paths
-  # blog.prefix = "blog"
-
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
-  # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
-  # blog.taglink = "tags/{tag}.html"
   blog.layout = "blog_post"
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
-  # blog.year_link = "{year}.html"
-  # blog.month_link = "{year}/{month}.html"
-  # blog.day_link = "{year}/{month}/{day}.html"
-  # blog.default_extension = ".markdown"
-
-  # blog.tag_template = "tag.html"
-  # blog.calendar_template = "calendar.html"
-
-  # Enable pagination
-  # blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/{num}"
 end
 
 page "/feed.xml", layout: false
 
-###
-# Compass
-###
+# dev mode addons
+activate :livereload
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+# for build
+activate :syntax
+activate :directory_indexes
 
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", layout: false
-#
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# activate :livereload
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-set :css_dir, 'stylesheets'
-
-set :js_dir, 'javascripts'
-
+set :css_dir,    'stylesheets'
+set :js_dir,     'javascripts'
 set :images_dir, 'images'
+set :frontmatter_extensions, %w(.html .slim)
 
-# Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
+  activate :minify_javascript
+end
 
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+# for deploy
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.build_before = true
+  deploy.branch = 'master'
 end
